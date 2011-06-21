75fad28295de51b81f82e966d777bc0eeb670113
Miguel Araujo Perez <miguel.araujo.perez@gmail.com>
2011-06-21 21:24:16 +0200

    Removing debugging lines and adding some white spaces to let the code breathe :)
    This code is not alpha state, is at least beta state or stable

--- mezzanine_superfish/templatetags/mezzanine_superfish_tags.py
+++ mezzanine_superfish/templatetags/mezzanine_superfish_tags.py
@@ -17,28 +17,33 @@ def superfish_submenu(context, token):
     Return a list of child pages for the given parent, storing all
     pages in a dict in the context when first called using parents as keys
     for retrieval on subsequent recursive calls from the menu template.
+
+    {% superfish page %}
+    {% superfish "templates/whatever.html" %}
+    {% superfish page "templates/whatever.html" %}
     """
-    # First arg could be the menu template file name, or the parent page.
+    # First arg could be the menu template file name or the parent page.
     # Also allow for both to be used.
-#    logging.debug('superfish_submenu')
+
     template_name = None
     parent_page = None
     parts = token.split_contents()[1:]
+
     for part in parts:
         part = Variable(part).resolve(context)
         if isinstance(part, unicode):
             template_name = part
         elif isinstance(part, Page):
             parent_page = part
+
     if template_name is None:
         try:
             template_name = "mezzanine_superfish/superfishtree.html"
-#            template_name = context["menu_template_name"]
         except KeyError:
             error = "No template found for page_menu in: %s" % parts
             raise TemplateSyntaxError(error)
     context["menu_template_name"] = template_name
-#    logging.debug("context['menu_template_name'] is " + context["menu_template_name"])
+
     if "menu_pages" not in context:
         pages = defaultdict(list)
         try:
@@ -52,6 +57,7 @@ def superfish_submenu(context, token):
             pages[page.parent_id].append(page)
         context["menu_pages"] = pages
         context["on_home"] = slug == reverse("home")
+
     # ``branch_level`` must be stored against each page so that the
     # calculation of it is correctly applied. This looks weird but if we do
     # the ``branch_level`` as a separate arg to the template tag with the
@@ -61,9 +67,10 @@ def superfish_submenu(context, token):
     if parent_page is not None:
         context["branch_level"] = parent_page.branch_level + 1
         parent_page = parent_page.id
+
     context["page_branch"] = context["menu_pages"].get(parent_page, [])
     for i, page in enumerate(context["page_branch"]):
         context["page_branch"][i].branch_level = context["branch_level"]
-    t = get_template(template_name, context)
-#    logging.debug(context["page_branch"])
-    return t.render(context)
+
+    template = get_template(template_name, context)
+    return template.render(context)
